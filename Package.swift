444b47cc6d7aa22517227192fdd318978753d0f4
stefanspringer1 <stefan.springer@icloud.com>
2024-10-31 06:38:16 +0100

    using CodepointMacro 1.0.3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/stefanspringer1/FastReplace", from: "1.0.0"),
-        .package(url: "https://github.com/stefanspringer1/CodepointMacro", from: "1.0.1"),
+        .package(url: "https://github.com/stefanspringer1/CodepointMacro", from: "1.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
